a55df31f3bf3eec75d65d4246cd528ce0506e440
Atsuya Ide <kappa_deatsu@yahoo.co.jp>
2022-10-12 16:38:46 +0900

    add: Setup section in README-ja.md

--- setup.ps1
+++ setup.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy ByPass -Scope CurrentUser
 $MainFilePath=$(Join-Path "$(Convert-Path $pwd)" "main.ps1")
 $SourceFilePath="%windir%\System32\WindowsPowerShell\v1.0\powershell.exe"
 $Arguments="-ExecutionPolicy ByPass -NoExit -Command `"& '$MainFilePath'`""
-$ShortcutPath=$(Join-Path "$(Convert-Path $pwd)" "LILY.lnk")
+$ShortcutPath=$(Join-Path "$(Convert-Path $pwd)" "Lily.lnk")
 
 $WScriptObj=New-Object -ComObject ("WScript.Shell")
 $shortcut=$WscriptObj.CreateShortcut($ShortcutPath)
