5751759fc4c450306c4ba7a2c9115839370e6aa3
Atsuya Ide <kappa_deatsu@yahoo.co.jp>
2022-10-12 15:52:42 +0900

    fix: setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,14 @@
+Set-ExecutionPolicy ByPass -Scope CurrentUser
+
 $MainFilePath=$(Join-Path "$(Convert-Path $pwd)" "main.ps1")
-$SourceFilePath="%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command `"& '$MainFilePath'`""
+$SourceFilePath="%windir%\System32\WindowsPowerShell\v1.0\powershell.exe"
+$Arguments="-ExecutionPolicy ByPass -NoExit -Command `"& '$MainFilePath'`""
+$ShortcutPath=$(Join-Path "$(Convert-Path $pwd)" "LILY.lnk")
 
 $WScriptObj=New-Object -ComObject ("WScript.Shell")
-$ShortcutPath=$(Join-Path "$(Convert-Path $pwd)" "LILY.lnk")
 $shortcut=$WscriptObj.CreateShortcut($ShortcutPath)
 $shortcut.TargetPath=$SourceFilePath
+$shortcut.Description="Lily (Powershell Prompt)"
+$shortcut.WorkingDirectory="%HOMEPATH%"
+$shortcut.Arguments=$Arguments
 $shortcut.Save()
