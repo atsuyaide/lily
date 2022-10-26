3c7db045de6427e377696912ec36596a018c3347
Atsuya Ide <kappa_deatsu@yahoo.co.jp>
2022-10-26 23:36:32 +0900

    update: manual

--- src/manual.ps1
+++ src/manual.ps1
@@ -4,28 +4,28 @@
 #>
 
 # If help-content is missing or Synopsis is not set, it will not appear in the manual listing
-Write-Output "==== Commands ===="
+$out = ""
+$out += "==== Commands ====`n"
 Get-ChildItem "$env:_SRC\[a-zA-Z]`*.ps1" -File | Foreach-Object {
-    $fileName = $_.BaseName
-    $helpObject = Get-Help $fileName
-    if ($helpObject.GetType().Name -ne "String" -and $helpObject.SYNOPSIS -ne "") {
-        $SYNOPSIS = $helpObject.SYNOPSIS
-        Write-Output $fileName
-        Write-Output " $SYNOPSIS"
+    $commandName = $_.BaseName
+    $helpObject = Get-Help $commandName
+    $SYNOPSIS = $helpObject.SYNOPSIS
+    if ($helpObject.GetType().Name -ne "String" -and $SYNOPSIS -notmatch "`n") {
+        $out += "$commandName`n $SYNOPSIS`n"
     }
 }
-Write-Output ""
+$out += "`n"
 
 # Aliases with no comments listed will not appear in the manual listing
-Write-Output "==== Aliases ===="
-$(Get-ChildItem function:)| foreach-object {
+$out += "==== Aliases ===="
+Get-ChildItem function: | foreach-object {
     if ($_.Source -eq "Lily") {
-        $helpObject = Get-Help $_.Name
-        if ($helpObject.SYNOPSIS -notmatch "`n") {
-            $SYNOPSIS = $helpObject.SYNOPSIS
-            Write-Output $_.Name
-            Write-Output " $SYNOPSIS"
+        $aliasName = $_.Name
+        $helpObject = Get-Help $_
+        $SYNOPSIS = $helpObject.SYNOPSIS
+        if ($SYNOPSIS -notmatch "`n") {
+            $out += "`n$aliasName`n $SYNOPSIS"
         }
     }
 }
-# Write-Output $(pdoskey /macros | Sort-Object | Select-String ";#=#" | ForEach-Object { $_ -replace "^(.*?)=(.*?);#=#", "`$1;#=#" } | ForEach-Object { $($_ -split";#=#")[0, -1] })
+Write-Output $out
